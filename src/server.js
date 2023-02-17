ac30de8b3c30fa326d0a7ad4e0e886633d7876c6
Sandro Tushurashvili <sandrotushurashvili7@gmail.com>
2023-02-17 18:53:01 +0400

    feat:Create swagger on localhost

--- src/server.js
+++ src/server.js
@@ -4,11 +4,13 @@ import dotenv from "dotenv";
 
 import connectToMongo from "./config/mongo.js";
 import productRouter from "./routes/product-router.js";
+import swaggerMiddleware from "./middlewares/swagger-middleware.js";
 
 const app = express();
 dotenv.config();
 connectToMongo();
 app.use(bodyParser.json());
 app.use("/api", productRouter);
+app.use("/", ...swaggerMiddleware());
 
 app.listen(3000);
