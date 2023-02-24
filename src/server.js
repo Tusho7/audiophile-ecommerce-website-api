dc05217e0e36dd2c9d6403fb740f481f083859c4
Sandro Tushurashvili <sandrotushurashvili7@gmail.com>
2023-02-24 23:21:34 +0400

    feat:add cors

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import bodyParser from "body-parser";
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import connectToMongo from "./config/mongo.js";
 import productRouter from "./routes/product-router.js";
@@ -11,6 +12,7 @@ const app = express();
 dotenv.config();
 connectToMongo();
 app.use(bodyParser.json());
+app.use(cors());
 app.use("/images", express.static("public/avatar"));
 app.use("/api", productRouter);
 app.use("/api", userRouter);
