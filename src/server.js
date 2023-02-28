afe478770a251372bd11fef203f24e4c5f18ee4c
Sandro Tushurashvili <sandrotushurashvili7@gmail.com>
2023-02-28 15:11:18 +0400

    refactor: Change folder direction

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ connectToMongo();
 app.use(bodyParser.json());
 app.use(cors());
 app.use("/images", express.static("public/avatar"));
-app.use("/allImages", express.static("public/AllImages"));
+app.use("/allImages", express.static("public/Images"));
 app.use("/api", productRouter);
 app.use("/api", userRouter);
 app.use("/", ...swaggerMiddleware());
