10d319ba41a09465f491a9dada81cfeca275a068
Sandro Tushurashvili <sandrotushurashvili7@gmail.com>
2023-02-28 15:03:55 +0400

    feat:Add images folder and change _id to id in products response

--- src/controllers/products-controller.js
+++ src/controllers/products-controller.js
@@ -2,5 +2,35 @@ import Product from "../models/products.js";
 
 export const getAllProducts = async (req, res) => {
   const data = await Product.find();
-  return res.json(data);
+  const newData = data.map((document) => {
+    return {
+      id: document.id,
+      slug: document.slug,
+      name: document.name,
+      image: document.image,
+      category: document.category,
+      categoryImage: document.categoryImage,
+      new: document.new,
+      price: document.price,
+      description: document.description,
+      features: document.features,
+      includes: document.includes.map((include) => {
+        return {
+          id: include.id,
+          quantity: include.quantity,
+          item: include.item,
+        };
+      }),
+      gallery: document.gallery,
+      others: document.others.map((other) => {
+        return {
+          id: other.id,
+          slug: other.slug,
+          name: other.name,
+          image: other.image,
+        };
+      }),
+    };
+  });
+  return res.json(newData);
 };
--- src/server.js
+++ src/server.js
@@ -14,6 +14,7 @@ connectToMongo();
 app.use(bodyParser.json());
 app.use(cors());
 app.use("/images", express.static("public/avatar"));
+app.use("/allImages", express.static("public/AllImages"));
 app.use("/api", productRouter);
 app.use("/api", userRouter);
 app.use("/", ...swaggerMiddleware());
