eca48a4a99040a8d1354b02d6bd7db361cd743dd
Sandro Tushurashvili <sandrotushurashvili7@gmail.com>
2023-03-21 17:15:36 +0400

    feat: Change cart controller

--- src/controllers/cart.js
+++ src/controllers/cart.js
@@ -1,37 +1,36 @@
 import CartItem from "../models/cartItem.js";
 
 export const postCart = async (req, res) => {
-  const cartitem = await CartItem.findOne({
-    userId: req.body.userId,
-    name: req.body.name,
-  });
+  const { userId, name, price, number, image } = req.body;
+  const cartitem = await CartItem.findOne({ userId, name, image });
   if (!cartitem) {
     const newCart = new CartItem({
-      name: req.body.name,
-      price: req.body.price,
-      number: req.body.number,
-      image: req.body.image,
-      userId: req.body.userId,
+      name,
+      price,
+      number,
+      image,
+      userId,
     });
     const saveCart = await newCart.save();
     return res.status(201).json("Cart Added");
   } else {
-    await cartitem.update({ number: cartitem.number + req.body.number });
+    await cartitem.updateOne({ $inc: { number } });
     return res.status(200).json("Cart Updated");
   }
 };
 
 export const getCart = async (req, res) => {
-  const allCarts = await CartItem.find();
+  const userId = String(req.params.id);
+  const allCarts = await CartItem.find({ userId });
   res.status(200).json(allCarts);
 };
 
 export const deleteCarts = async (req, res) => {
   try {
-    const userId = String(req.params.id);
-    console.log("userId:", userId); // Log the userId value
+    const userId = String(req.user._id);
+    console.log("userId:", userId);
     const result = await CartItem.deleteMany({ userId });
-    console.log("result:", result); // Log the result of the deleteMany operation
+    console.log("result:", result);
     res.status(200).json({ message: "Cart items successfully deleted" });
   } catch (error) {
     console.log(error);
